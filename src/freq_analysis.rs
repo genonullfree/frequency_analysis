58493cb6420ca20588ed53c0db07772022d038b2
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-07-28 21:37:12 -0400

    have_vowels

--- src/freq_analysis.rs
+++ src/freq_analysis.rs
@@ -4,3 +4,17 @@ pub fn printable_ascii(a: Vec<u8>) -> bool {
 
     s.is_ascii()
 }
+
+pub fn have_vowels(a: Vec<u8>) -> bool {
+    let vowels: [char; 6] = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+    for i in &vowels {
+        for b in &a {
+            if *i == *b as char {
+                return true;
+            }
+        }
+    }
+
+    false
+}
